e8468f0631828d26b8ede03298f732b50a67f714
Chitsutha Soomlek <46390758+ChitsuthaCSKKU@users.noreply.github.com>
2021-08-16 16:48:07 +0700

    Add Lab6
    Lab 6 Decision Table

---
+++ LabDemo/Lab6_DT/main/PhonePlan.java
@@ -0,0 +1,20 @@
+package sqa;
+
+public class PhonePlan {
+	
+	private final boolean international, autoRenewal, loyal;
+
+    public PhonePlan(boolean international, boolean autoRenewal, boolean loyal) {
+        this.international = international;
+        this.autoRenewal = autoRenewal;
+        this.loyal = loyal;
+    }
+
+    int pricePerMonth() {
+        boolean isStandard = !(loyal || autoRenewal);
+        return international
+                ? isStandard ? 32 : 30
+                : isStandard ? 15 : 10;
+    }
+
+}
